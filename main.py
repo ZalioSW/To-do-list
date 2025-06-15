2976ab735e210c8ea5311599fd4fb6af1984f446
ZalioSW <ztoxiumisntoxic@gmail.com>
2025-06-15 10:51:26 +0200

    setting up login and signup part 1

--- main.py
+++ main.py
@@ -1,39 +1,47 @@
 from flask import Flask, session, render_template, jsonify, redirect, url_for, request
 import sqlite3
 
-conn = sqlite3.connect('uporabniki')
+conn = sqlite3.connect('uporabniki', check_same_thread=False)
 kurzor = conn.cursor()
+kurzor.execute("DROP TABLE IF EXISTS uporabniki")
 kurzor.execute('''
     CREATE TABLE IF NOT EXISTS uporabniki(
-        ID INT PRIMARY KEY NOT NULL ,
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
         Username VARCHAR(100),
         Email VARCHAR(100),
         Password TEXT NOT NULL
     )
 ''')
 conn.commit()
-conn.close()
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home_page():
     if 'username' not in session:
         return render_template("signup.html")
     return render_template("index.html")
     
-@app.route("/signup", methods = ["GET", "POST"])
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-    username = request.form["username"]
-    email = request.form["email"]
-    password = request.form["password"]
-    print(username)
-    print(email)
-    print(password)
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        if not username or not password:
+            return jsonify({"success": False, "error": "Username and password are required."})
+        kurzor.execute("""INSERT INTO uporabniki (Username, Email, Password) VALUES (:username, :email, :password)""", {"username": username, "email": email, "password": password})
+        conn.commit()
+        return jsonify({"success": True})
     return render_template("signup.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        kurzor.execute("SELECT Username, Password FROM uporabniki WHERE username = :username AND password = :password", {"username": username, "password": password})
+        return jsonify({"success": True})
     return render_template("login.html")
 
 app.run(debug=True)
